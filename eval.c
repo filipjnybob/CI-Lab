f586a476d7f553a7336fd2f9ddb43ae128183cbe
Isaac Adams <ia81718@gmail.com>
2021-02-03 22:27:20 -0600

    Added functions for subtracting, dividing, and modulo Began work on the multiply function.

--- eval.c
+++ eval.c
@@ -14,24 +14,32 @@
 extern bool is_binop(token_t);
 extern bool is_unop(token_t);
 char *strrev(char *str);
+static void add(value_t *value, node_t *left, node_t *right);
+static void subtract(value_t *value, node_t *left, node_t *right);
+static void multiply(value_t *value, node_t *left, node_t *right);
+static void divide(value_t *value, node_t *left, node_t *right);
+static void modulo(value_t *value, node_t *left, node_t *right);
+
+// TODO TODO****: Paste new tarball files from canvas into the directory
 
 // Lists valid types for each binop operator
 // Can access the correct list by calling binopTypes[(Token) - TOK_PLUS]
 static const struct {
     token_t binopTok;
+    void (*func_ptr)(value_t *value, node_t *left, node_t *right);
     int numValid;
     node_type_t types[2];
 } binopTypes[] = {
-    {TOK_PLUS,   2,    {INT_TYPE, STRING_TYPE}},              // +
-    {TOK_BMINUS, 1,    {INT_TYPE}},                           // -
-    {TOK_TIMES,  2,    {INT_TYPE, STRING_TYPE}},              // *
-    {TOK_DIV,    1,    {INT_TYPE}},                           // /
-    {TOK_MOD,    1,    {INT_TYPE}},                           // %
-    {TOK_AND,    1,    {BOOL_TYPE}},                          // &
-    {TOK_OR,     1,    {BOOL_TYPE}},                          // |
-    {TOK_LT,     2,    {INT_TYPE, STRING_TYPE}},              // <
-    {TOK_GT,     2,    {INT_TYPE, STRING_TYPE}},              // >
-    {TOK_EQ,     2,    {INT_TYPE, STRING_TYPE}}               // ~
+    {TOK_PLUS,   &add,  2,    {INT_TYPE, STRING_TYPE}},              // +
+    {TOK_BMINUS, &subtract, 1,    {INT_TYPE}},                           // -
+    {TOK_TIMES,  &multiply, 2,    {INT_TYPE, STRING_TYPE}},              // *
+    {TOK_DIV,    &divide, 1,    {INT_TYPE}},                           // /
+    {TOK_MOD,    &modulo, 1,    {INT_TYPE}},                           // %
+    {TOK_AND,    NULL, 1,    {BOOL_TYPE}},                          // &
+    {TOK_OR,     NULL, 1,    {BOOL_TYPE}},                          // |
+    {TOK_LT,     NULL, 2,    {INT_TYPE, STRING_TYPE}},              // <
+    {TOK_GT,     NULL, 2,    {INT_TYPE, STRING_TYPE}},              // >
+    {TOK_EQ,     NULL, 2,    {INT_TYPE, STRING_TYPE}}               // ~
 };
 
 /* infer_type() - set the type of a non-root node based on the types of children
@@ -100,7 +108,10 @@ static void infer_root(node_t *nptr) {
     return;
 }
 
-static void add(value_t *value,node_t *left, node_t *right) {
+/* add() - Adds the values of the left and right nodes based on their type and 
+ * stores the result in value
+ */
+static void add(value_t *value, node_t *left, node_t *right) {
     if(left->type != right->type) {
         handle_error(ERR_TYPE);
         return;
@@ -126,6 +137,84 @@ static void add(value_t *value,node_t *left, node_t *right) {
     return;
 }
 
+// subtract() - Subtracts the int value of right from left and stores it in value.
+static void subtract(value_t *value, node_t *left, node_t *right) {
+    if(left->type != INT_TYPE || right->type != INT_TYPE) {
+        handle_error(ERR_TYPE);
+        return;
+    }
+
+    value->ival = left->val.ival - right->val.ival;
+    return;
+}
+
+// multiply() - TODO
+static void multiply(value_t *value, node_t *left, node_t *right) {
+    if(right->type != INT_TYPE) {
+        handle_error(ERR_TYPE);
+        return;
+    }
+
+    if(left->type == INT_TYPE) {
+        value->ival = left->val.ival * right->val.ival;
+        return;
+    }
+
+    if(left->type == STRING_TYPE) {
+        value->sval = (char *) malloc(strlen(left->val.sval) + strlen(right->val.sval) + 1);
+        if (! value->sval) {
+            logging(LOG_FATAL, "failed to allocate string");
+            return;
+        }
+                        
+        strcpy(value->sval, left->val.sval);
+        strcat(value->sval, right->val.sval);
+        return;
+    }
+
+    handle_error(ERR_TYPE);
+    return;
+}
+
+/* divide() - Divides the value of left by the value of right
+ * and stores it in value. Causes an evaluation error if dividing
+ * by 0.
+ */
+static void divide(value_t *value, node_t *left, node_t *right) {
+    if(left->type != INT_TYPE || right->type != INT_TYPE) {
+        handle_error(ERR_TYPE);
+        return;
+    }
+
+    if(right->val.ival == 0) {
+        handle_error(ERR_EVAL);
+        return;
+    }
+
+    value->ival = left->val.ival / right->val.ival;
+    return;
+}
+
+/* modulo() - Returns the modulo of the left value by the right value
+ * and  stores it in vale. Causes an evaluation error if modulo by 0.
+ */
+static void modulo(value_t *value, node_t *left, node_t *right) {
+    if(left->type != INT_TYPE || right->type != INT_TYPE) {
+        handle_error(ERR_TYPE);
+        return;
+    }
+
+    if(right->val.ival == 0) {
+        handle_error(ERR_EVAL);
+        return;
+    }
+
+    value->ival = left->val.ival % right->val.ival;
+    return;
+}
+
+
+
 /* eval_node() - set the value of a non-root node based on the values of children
  * Parameter: A node pointer, possibly NULL.
  * Return value: None.
@@ -138,19 +227,18 @@ static void eval_node(node_t *nptr) {
     if(terminate || ignore_input) return;
 
     if(nptr->node_type == NT_INTERNAL) {
+
+        // Handle binary operators
         if(is_binop(nptr->tok)) {
             for(int i = 0; i < 2; i++) {
                 eval_node(nptr->children[i]);
             }
 
             if(terminate || ignore_input) return;
-
-            switch(nptr->tok) {
-                case TOK_PLUS:
-                    add(&nptr->val, nptr->children[0], nptr->children[1]);
-                    break;
-            }
-
+            
+            // Call the appropriate function based on the token
+            int index = nptr->tok - TOK_PLUS;
+            (*binopTypes[index].func_ptr)(&nptr->val, nptr->children[0], nptr->children[1]);
         }
     }
 
